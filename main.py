14e1f5e7b29644acd44da1d197e5aa0335c07948
shaharbn <shahar.barnatan@gmail.com>
2024-11-16 16:02:45 +0200

    show player on the screan

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
+        player.draw(screen)
+        
         dt = fps.tick(60) / 1000
+        pygame.display.flip()
+        
 
 
 
