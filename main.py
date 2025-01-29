491bd880ff77e4a1d7b8f8b0148d7d626f1b1b4b
shaharbn <shahar.barnatan@gmail.com>
2025-01-29 17:46:05 +0200

    added 60 fps and circle class

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    #delta time berwin drawing the last game state
+    time = pygame.time.Clock()
+    dt = 0
     # the gui
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # game loop
@@ -21,8 +24,10 @@ def main():
         # step 3 Draw the game to the screen
         screen.fill("black")
 
-        # refrash the screen last comand
+        # refrash the screen last comand of step 3
         pygame.display.flip()
+        #make the loop w8 for 1/60 sec and return in seconds when the last time its have been called
+        dt = time.tick(60) / 1000
 
 
 
